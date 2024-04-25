43c130cc28ace0d96412582b38ade80da1a0eeb1
KaterinaMitsioni <146973369+KaterinaMitsioni@users.noreply.github.com>
2024-04-25 21:07:27 +0300

    teliko

--- assignment-2024-1/tromino_tiling.py
+++ assignment-2024-1/tromino_tiling.py
@@ -6,27 +6,58 @@ def tromino(n):
         matrix[1][0]="G"
         matrix[1][1]="G"
     elif n== 2:
-        matrix[0][0]="Β"
-        matrix[1][0]="Β"
-        matrix[0][1]="Β"
-        matrix[0][2]="R"
-        matrix[0][3]="R"
-        matrix[1][3]="R"
-        matrix[2][0]="R"
-        matrix[3][0]="R"
-        matrix[3][1]="R"
-        matrix[2][2]="Β"
-        matrix[3][2]="Β"
-        matrix[2][3]="Β"
-        matrix[1][1]="G"
-        matrix[2][1]="G"
-        matrix[1][2]="G"
-    elif n > 2:
-        center_down_left = (2**n)//2
-        matrix[center_down_left][center_down_left] = "G"
-        matrix[center_down_left][center_down_left-1] = "G"
-        matrix[center_down_left-1][center_down_left] = "G"
-        
+        matrix = [["B", "B", "R", "R"],
+                  ["B", "G", "G", "R"],
+                  ["R", "G", "B", "B"],
+                  ["R", "R", "B", "X"]]
 
+    elif n == 3:
+        matrix = [["B", "B", "R", "R"],
+                  ["B", "G", "G", "R"],
+                  ["R", "G", "B", "B"],
+                  ["R", "R", "B", "X"]]
+        def rotate_90(matrix):
+             transposed = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+             rotated_matrix = [row[::-1] for row in transposed]
+             return rotated_matrix
+        matrix2 = rotate_90(matrix)
+        def replace_colors(matrix):
+            for i in range(len(matrix)):
+                for j in range(len(matrix[0])):
+                    if matrix[i][j] == "R":
+                        matrix[i][j] = "B"
+                    elif matrix[i][j] == "B":
+                        matrix[i][j] = "R"
+        
+        replace_colors(matrix2)
+        def rotate_180(matrix):
+            rotated_matrix180 = matrix
+            for i in range(2):
+                rotated_matrix180 = rotate_90(rotated_matrix180)
+            return rotated_matrix180
+        matrix3 = rotate_180(matrix)
+        def rotate_270(matrix):
+            rotated_matrix270 = matrix
+            for i in range(3):
+                rotated_matrix270 = rotate_90(rotated_matrix270)
+            return rotated_matrix270
+        matrix4 = rotate_270(matrix)
+        replace_colors(matrix4)
+
+        merged_matrix = []
+        # Ενώνουμε τον δεύτερο πίνακα στον πρώτο
+        for i in range(len(matrix)):
+            merged_matrix.append(matrix[i] + matrix2[i])
+        for row in range(len(matrix4)):
+            merged_matrix.append(matrix4[row] ) 
+        for i in range(len(matrix3)):
+            merged_matrix[len(matrix) + i] += matrix3[i]
+        matrix = merged_matrix
+        
+  
+    
     for row in matrix:
         print(row)
+        
+tromino(3)
+
